1431ace837f6a45d4ae7cc9f4728f3a28a8f5900
farobledo <fabian.robledolave@gmail.com>
2022-04-07 17:45:33 -0500

    html css

---
+++ components/db.php
@@ -0,0 +1,14 @@
+<?php
+
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+$db_name = 'test';
+
+
+$db = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+
+if ($db->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
+}
